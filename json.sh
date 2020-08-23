a892a8fc9843a00365cf143d8523fd65a21f2094
RunsetTech <52034765+RunsetTech@users.noreply.github.com>
2020-08-23 20:00:53 +0430

    Update json.sh

--- json.sh
+++ json.sh
@@ -5,6 +5,9 @@
 #
 # Modified by EchoEkhi to use with automated software in 2020.
 
+IPP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+export ENDERPOINT=$IPP
+
 exec < <(exec wg show all dump)
 
 printf '{ '
@@ -14,7 +17,7 @@ while read -r -d $'\t' device; do
 		printf '%s\t{\n' "$delim" 
 		delim=$'\n'
 		{ printf '%s\t\t"publicKey": "%s"' '' "$private_key"; delim=$',\n'; }
-		{ printf '%s\t\t"endpoint": "%s"' "$delim"  "$SERVER_HOST:$preshared_key"; delim=$',\n'; }
+		{ printf '%s\t\t"endpoint": "%s"' "$delim"  "$ENDERPOINT:$preshared_key"; delim=$',\n'; }
 		{ printf '%s\t\t"latestHandshake": %u' "$delim" $(( $latest_handshake )); delim=$',\n'; }
 		{ printf '%s\t\t"upload": %u' "$delim" $(( $transfer_rx )); delim=$',\n'; }
 		{ printf '%s\t\t"download": %u' "$delim" $(( $transfer_tx )); delim=$'\n'; }
